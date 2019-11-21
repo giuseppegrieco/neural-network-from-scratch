eef9836733501bbc576fae786b0e62124321d362
Mattia Sangermano <mattiasangermano1997@gmail.com>
2019-11-21 14:27:42 +0100

    - plot storing fixed

--- experiments/__main__.py
+++ experiments/__main__.py
@@ -5,16 +5,14 @@ import numpy as np
 import sys
 import random
 import string
+import os
 
 # Hyperparameters
 eta = 0.01
-lambda_reg = 0.001
+lambda_reg = 0.0001
 alpha_momentum = 0.1
 topology = [
-    nn.Layer(nodes=4, activation_function=nn.Sigmoid()),
-    nn.Layer(nodes=4, activation_function=nn.Sigmoid()),
-    nn.Layer(nodes=4, activation_function=nn.Sigmoid()),
-
+    nn.Layer(nodes=5, activation_function=nn.Sigmoid()),
     nn.Layer(nodes=1, activation_function=nn.Sigmoid())
 ]
 
@@ -27,6 +25,19 @@ nn = nn.NeuralNetwork(
         alpha_momentum=alpha_momentum
     )
 )
+
+
+def computes_accuracy(targets, expected_output):
+    res = []
+    for i in np.nditer(targets):
+        if i > 0.5:
+            res.append(1.0)
+        else:
+            res.append(0.0)
+
+    all_wrong_output = np.matrix.sum(abs(np.mat(res) - expected_output))
+    m, n = vt.shape
+    return 1.0 - (all_wrong_output / n)
 
 
 def file_parser(file_name, input_list, output_list):
@@ -54,43 +65,42 @@ tr_output = []
 ts_input = []
 ts_output = []
 
-file_parser('monks-1.train', tr_input, tr_output)
-file_parser('monks-1.test', ts_input, ts_output)
+file_parser('monks-2.train', tr_input, tr_output)
+file_parser('monks-2.test', ts_input, ts_output)
 
 # wrong way !!!!
-terrors = []
-verrors = []
+tr_errors = []
+v_errors = []
+v_accuracy = []
+vt = 0
+expected_output = 0
 
 tr_input = np.mat(tr_input).T.tolist()
 ts_input = np.mat(ts_input).T.tolist()
-#tr_input = [[0, 0, 1, 1],[0, 1, 0, 1]]
-#tr_output = [0, 1, 1, 0]
+# tr_input = [[0, 0, 1, 1],[0, 1, 0, 1]]
+# tr_output = [0, 1, 1, 0]
 for i in range(1, 10000):
     current_verror = 0
     current_terror = 0
     # vect = list(range(len(tr)))
     # random.shuffle(vect)
 
-    terrors.append(nn.train(tr_input, tr_output))
+    tr_errors.append(nn.train(tr_input, tr_output))
 
     vt = nn.feed_forward(ts_input)
     expected_output = np.mat(ts_output)
 
-    verrors.append(
-       np.matrix.sum(np.power(expected_output - vt, 2)) * 1 / len(expected_output.T)
+    v_errors.append(
+        np.matrix.sum(np.power(expected_output - vt, 2)) * 1 / len(expected_output.T)
     )
+    # Calcolo accuracy
+    # v_accuracy.append(computes_accuracy(vt,expected_output))
 
-    #for j in range(0, len(ts_input)):
-        #current_verror = current_verror + np.power(ts_output[j] - nn.feed_forward(ts_input[j]).item(0), 2)
-    #current_verror = 1 / len(ts_input) * current_verror
-    #verrors.append(current_verror)
+print(computes_accuracy(vt, expected_output))
 
-tr_input = np.mat(tr_input)
-print(nn.feed_forward(tr_input[:,4].tolist()))
-print(nn.feed_forward(tr_input[:,10].tolist()))
 fig, subplot = plt.subplots(nrows=1, ncols=1)
-subplot.plot(terrors, '-b', label='Training')
-subplot.plot(verrors, '--r', label='Validation')
+subplot.plot(tr_errors, '-b', label='Training')
+subplot.plot(v_errors, '--r', label='Validation')
 subplot.legend()
 
 props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
@@ -104,7 +114,7 @@ subplot.text(0.695, 0.8, text_str, transform=subplot.transAxes, fontsize=10,
 
 all_char = string.ascii_letters + string.digits
 graph_name = "".join(random.choice(all_char) for x in range(random.randint(8, 12)))
+my_path = os.path.dirname(__file__)
+fig.savefig(os.path.dirname(my_path) + "/charts/" + graph_name)
 plt.show()
-fig.savefig(graph_name)
-
 sys.exit()
