41b522e9510bad1eb6bab2e8a5a52916466ac577
Mattia Sangermano <mattiasangermano1997@gmail.com>
2020-01-20 13:00:41 +0100

    changed input

--- experiments/__main__.py
+++ experiments/__main__.py
@@ -169,7 +169,7 @@ if __name__ == '__main__':
             ]
 
         ],
-        learning_rate_list=[0.000001, 0.0000001],
+        learning_rate_list=[0.0005, 0.0002, 0.0001, 0.00001],
         momentum_list=[0.9, 0.6, 0.3, 0.1, 0],
         epochs_list=[15000],
         regularization_list=[0.01, 0.001, 0.0001, 0.00001, 0.000001]
@@ -194,7 +194,7 @@ if __name__ == '__main__':
 
     initial_path = create_timestamp_directory("./grid_search/", "GS-")
 
-    grid_result = gs.run(2, X_train, Y_train, save_result)
+    grid_result = gs.run(4, X_train, Y_train, save_result)
 
     end_time_GS = datetime.datetime.now().timestamp()
     grid_search_duration_in_sec = end_time_GS - start_time_GS
