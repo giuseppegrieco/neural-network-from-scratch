fc84947043c41761ba7d8f1cd3ccaf976fc549da
Giuseppe Grieco <g.grieco1997@gmail.com>
2020-01-20 10:22:57 +0100

    new version of library

--- neural_network/hyperparameter_tuning/grid_search.py
+++ neural_network/hyperparameter_tuning/grid_search.py
@@ -1,5 +1,4 @@
-from concurrent.futures.process import ProcessPoolExecutor
-from concurrent.futures.thread import ThreadPoolExecutor
+from multiprocessing.pool import Pool
 
 import numpy as np
 
@@ -13,25 +12,28 @@ class GridSearch(object):
         self.__cross_validation = cross_validation
 
     def run(self, number_of_process: int, X_train: np.mat, Y_train: np.mat):
-        with ProcessPoolExecutor(max_workers=number_of_process) as executor:
+        with Pool(number_of_process) as executor:
             futures = []
             for hyperparameters in self.__grid_search_specification.combinations_of_hyperparameters():
-                futures.append((executor.submit(
+                futures.append((executor.map(
                     self.__cross_validation.estimates,
-                    self.__grid_search_specification.build_neural_network_object(
-                        hyperparameters
-                    ),
-                    self.__grid_search_specification.build_learning_algorithm_object(
-                        hyperparameters
-                    ),
-                    X_train,
-                    Y_train
+                    (
+                        self.__grid_search_specification.build_neural_network_object(
+                            hyperparameters
+                        ),
+                        self.__grid_search_specification.build_learning_algorithm_object(
+                            hyperparameters
+                        ),
+                        X_train,
+                        Y_train
+                    )
                 ), hyperparameters))
-            executor.shutdown(wait=True)
+            executor.close()
+            executor.join()
             results = []
             for future in futures:
                 results.append({
-                    'result': future[0].result(),
+                    'result': future[0],
                     'hyperparameters': self.__grid_search_specification.combinations_repr(future[1])
                 })
             return results
