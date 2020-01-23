b609001234e2c878ee5b62404e414f35e5c08636
Mattia Sangermano <mattiasangermano1997@gmail.com>
2020-01-23 21:00:57 +0100

    changed input

--- experiments/__main__.py
+++ experiments/__main__.py
@@ -204,7 +204,7 @@ if __name__ == '__main__':
             ]
 
         ],
-        learning_rate_list=[0.5, 0.2, 0.1, 0.01],
+        learning_rate_list=[0.001,0.0001],
         momentum_list=[0.9, 0.6, 0.3, 0.1, 0],
         epochs_list=[15000],
         regularization_list=[0.01, 0.001, 0.0001, 0.00001, 0.000001]
