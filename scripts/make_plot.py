c68bd5d22101b529f3137ad38b2eefd7c5fd61d0
Giuseppe Grieco <g.grieco1997@gmail.com>
2020-01-24 16:45:10 +0100

    make plot script

--- scripts/make_plot.py
+++ scripts/make_plot.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def plotgraph(training_errors, validation_errors, eta, alpha, lam, fn, sn):
+def plotgraph(directory, training_errors, validation_errors, eta, alpha, lam, fn, sn):
     fig, ax = plt.subplots()
     fig.set_figheight(4)
     fig.set_figwidth(10)
@@ -31,7 +31,7 @@ def plotgraph(training_errors, validation_errors, eta, alpha, lam, fn, sn):
             verticalalignment='top', bbox=props)
     plt.tight_layout()
 
-    plt.savefig('plot.svg', format="svg")
+    plt.savefig(directory + 'plot.svg', format="svg")
 
     plt.show()
 
@@ -54,6 +54,6 @@ for directory in os.listdir(file):
         for sub_dir in os.listdir(directory):
             tr_errors = np.load(sub_dir + "training_errors_mse.npy", allow_pickle=False)
             vl_errors = np.load(sub_dir + "validation_errors_mse.npy", allow_pickle=False)
-            plotgraph(tr_errors, vl_errors, lr, momentum, lambda_reg, topology, 2)
+            plotgraph(directory, tr_errors, vl_errors, lr, momentum, lambda_reg, topology, 2)
 
 sys.exit()
